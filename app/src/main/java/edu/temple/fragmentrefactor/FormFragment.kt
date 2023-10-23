7b5f45ba1713b0a6e8dbeee4d999dc57ad237ce9
Joe Michel <tun69277@temple.edu>
2023-10-23 16:13:32 -0400

    Added fragment container to main layout

--- app/src/main/java/edu/temple/fragmentrefactor/FormFragment.kt
+++ app/src/main/java/edu/temple/fragmentrefactor/FormFragment.kt
@@ -4,8 +4,29 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 
 class FormFragment: Fragment() {
-    override fun onCreate(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View?{}
+    override fun onCreate(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View?
+    {
+        val layout= inflater.inflate(R.layout.fragment_form, container, false)
+        val displayTextView = findViewById<TextView>(R.id.displayTextView)
+        val nameEditText = findViewById<EditText>(R.id.nameEditText)
+        val changeButton = findViewById<Button>(R.id.changeButton)
+
+        changeButton.setOnClickListener {
+            val name = nameEditText.text
+
+            displayTextView.text = if (name.isNotBlank()) {
+                "Hello, $name!"
+            } else {
+                "Please enter your name"
+            }
+        }
+
+        return layout
+    }
 }
