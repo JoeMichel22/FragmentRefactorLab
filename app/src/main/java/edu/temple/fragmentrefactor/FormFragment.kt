866aa59e8399f102cc454e61f553a89782e08202
Joe Michel <tun69277@temple.edu>
2023-10-23 16:01:07 -0400

    Create and added fragment layout and fragment class file

---
+++ app/src/main/java/edu/temple/fragmentrefactor/FormFragment.kt
@@ -0,0 +1,11 @@
+package edu.temple.fragmentrefactor
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class FormFragment: Fragment() {
+    override fun onCreate(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View?{}
+}
